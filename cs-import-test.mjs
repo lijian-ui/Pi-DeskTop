14b318161d8d7c6edf355936f3fdde62ce4a0715
lijian-ui <209336177+lijian-ui@users.noreply.github.com>
2026-08-07 14:28:03 +0800

    bug修复

--- cs-import-test.mjs
+++
@@ -1,2 +0,0 @@
-import { computeCacheWaste, detectCacheMiss } from "@earendil-works/pi-coding-agent";
-console.log("computeCacheWaste:", typeof computeCacheWaste, "detectCacheMiss:", typeof detectCacheMiss);
